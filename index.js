2d99b2d27ad5186f37a5778bfba77e6153d7cdb7
Kishan Mirajkar <kishanmirajkar@Kishans-MacBook-Air.local>
2023-08-02 10:44:04 +0530

    instaclone

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const postRoute = require("./Routes/route");
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+app.use(express.static("uploads"))
+const url =
+  "mongodb+srv://kishanmirajkar27:2Hbk0YhrZVvi4ctJ@cluster0.qwvyrjn.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.log("Error in connection", err);
+  });
+
+app.use("/", postRoute);
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
